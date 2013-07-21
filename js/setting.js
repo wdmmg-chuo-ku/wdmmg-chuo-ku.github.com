42a2a4bfade10da580252b7f01bb374f6f0b6eec
sa2hi <sa2.hiroyuki@gmail.com>
2013-07-21 11:17:31 +0900

    changed OpenSpending.identifier in js/setting.js to use Chuo-ku data, and changed category and subcategory label name (upper case -> lower) in index.html for adjusting registered openspending Chuo-ku data

--- js/setting.js
+++ js/setting.js
@@ -10,8 +10,8 @@ Taxes.taxRate = 0.06; // 住民税率
 
 var OpenSpending = OpenSpending || {};
 
-OpenSpending.identifier = 'yokohama_budget';
-OpenSpending.year = '2012';
+OpenSpending.identifier = 'chuo-ku_tokyo_2011_financial_statements';
+OpenSpending.year = '2011';
 
 OpenSpending.Styles = OpenSpending.Styles || {};
 
